e40e739a0555e5f2e0505a06347bf8c6bc7143b3
Jho2e <ohjaehokor@naver.com>
2022-12-07 16:08:39 +0900

    useNavigate 사용법

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = (url: string) => {
+    navigate(url);
+  };
+
   return (
     <header>
       <ul>
         <li>
-          <Link to={'/'}>Home</Link>
+          <button onClick={() => onAboutClick('/')}>Home</button>
         </li>
         <li>
-          <Link to={'/about'}>About</Link>
+          <button onClick={() => onAboutClick('/about')}>About</button>
         </li>
       </ul>
     </header>
   );
+
+  {
+    /* <Link> 를 사용
+  <header>
+  <ul>
+  <li>
+      <Link to={'/'}>Home</Link>
+    </li>
+    <li>
+      <Link to={'/about'}>About</Link>
+      </li>
+  </ul>
+  </header> 
+  */
+  }
 }
 
 export default Header;
